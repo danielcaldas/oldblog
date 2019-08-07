d3fbf84482e94cf29d31ff70d93d71a8325873d7
Daniel Caldas <11733994+danielcaldas@users.noreply.github.com>
2019-08-07 16:41:44 +0800

    Feature/about section overall improvements (#28)
    * Add htmlproofer step to travis scripts
    
    * Remove outdated link from article on tech job interviews
    
    * Configure html-proofer to run in travis
    
    * Remove spacing in 404.html
    
    * Add avatar image (square proportions)
    
    * Add aboutme page
    
    * Small update in aboutme.md
    
    * Add twitter handle to _config
    
    * Update LinkedIn URL
    
    * Move htmlproofer step to cibuild.sh
    
    * Add --disable-external flag to htmlproofer

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,4 +2,7 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --disable-external
+bundle exec htmlproofer ./_site\
+    --disable-external\
+    --http-status-ignore "999"\
+    --url-ignore "https://codepen.io/anon/pen/vrewpa"
