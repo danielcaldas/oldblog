c5fe731dfb20b8dac9ec2d257927059f1cdaba33
danielcaldas <caldasjdaniel@gmail.com>
2019-08-07 16:19:22 +0800

    Move htmlproofer step to cibuild.sh

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,4 +2,6 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --disable-external
+bundle exec htmlproofer ./_site\
+    --http-status-ignore "999"\
+    --url-ignore "https://codepen.io/anon/pen/vrewpa"
