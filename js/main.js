6343f37dfeb3db0cd25964daf2389210d5d38e95
danielcaldas <dcaldas@zendesk.com>
2020-03-31 15:54:37 +0800

    Add JavaScript to smootly jump to header anchors

--- js/main.js
+++ js/main.js
@@ -37,6 +37,20 @@ var main = {
       });
     });
 
+    // Animate scrolling to specific header anchor in any blog post
+    // @danielcaldas
+    var BEAUTIFUL_JEKYLL_HEADER_HEIGHT = 80;
+    $('.blog-post > h1, h2, h3, h4, h5').click(function (e) {
+      e.preventDefault();
+      try {
+        var offset = $(this).offset();
+        $("html, body").animate(
+          { scrollTop: offset.top - BEAUTIFUL_JEKYLL_HEADER_HEIGHT },
+          "fast"
+        );
+      } catch (error) {}
+    });
+
     // Ensure nested navbar menus are not longer than the menu header
     var menus = $(".navlinks-container");
     if (menus.length > 0) {
